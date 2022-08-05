48296645f1329323c4c9a9471e1a6b874d84eee9
Windson Macedo <windsondev@hotmail.com>
2022-08-05 18:15:26 -0300

    requirement 03 requirement 04

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-// import logo from './logo.svg';
 import './App.css';
-import Home from '../Pages/Home';
+import Home from './Pages/Home';
+import ShoppingCart from './Pages/ShoppingCart';
 
 export default class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Route exact path="/" component={ Home } />
+        <Route exact path="/ShoppingCart" component={ ShoppingCart } />
       </BrowserRouter>
     );
   }
